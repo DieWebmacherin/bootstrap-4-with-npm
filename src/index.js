0aae496af5e00dfdebf04f657cd974b24e7b07f8
vbryan <vladimir.bryantsev@gmail.com>
2018-08-28 16:36:32 +0300

    OTHER-1097: split webpack configurations and added new webpack modules

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './style.css';
 import './scss/custom.scss';
 
 function component() {
-  let element = document.createElement('div');
+    let element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
---
+++ webpack.dev.conf.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        disableHostCheck: true
+    },
+    module: {
+        rules: [{
+            test: /\.(html)$/,
+            include: path.join(__dirname, 'partials'),
+            use: {
+                loader: 'html-loader',
+                options: {
+                    interpolate: true
+                }
+            }
+        }, {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'postcss-loader',
+                'sass-loader',
+            ],
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Tether: 'tether',
+            'window.Tether': 'tether',
+            Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+            Button: 'exports-loader?Button!bootstrap/js/dist/button',
+            Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+            Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+            Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+            Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+            Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+            Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+            Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+            Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+            Util: 'exports-loader?Util!bootstrap/js/dist/util',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html'
+        }),
+    ],
+};
