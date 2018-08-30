e31d15f9b567cac07f21deaf7febfaab3bbd449a
Sara Break <sg@break-media.com>
2018-08-30 10:18:17 +0200

    OTHER-1097 Adding slick slider (two versions, styles, scripts)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import 'bootstrap';
 import _ from 'lodash';
-fontawesome.library.add(farCalendar, farEnvelope, fasArrowUp);
+import 'slick-carousel';
 import './scss/custom.scss';
+
+//----- Custom scripts -----//
+//Slick carousel
+$('.slick-carousel.fullwidth').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true
+});
+$('.slick-carousel.edge').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev headline5">&larr;</button>',
+    nextArrow: '<button type="button" class="slick-next headline5">&rarr;</button>'
+});
